2170f7c20f66d57de300025934c85a99986933e9
Natalia Jaguś <natalia.jagus@gmail.com>
2019-09-29 20:14:25 +0200

    create Controls component

---
+++ src/components/Controls/Controls.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+
+const Controls = () => (
+    <div>
+    <Button>Start</Button>
+      <Button secondary>Stop</Button>
+    </div>
+)
+
+
+export default Controls;

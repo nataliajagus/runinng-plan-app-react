b02580c171c37c7c77094495f177b65f7fb55626
Natalia Jaguś <natalia.jagus@gmail.com>
2019-09-29 20:15:02 +0200

    create Wrapper component

---
+++ src/components/Wrapper/Wrapper.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export default Wrapper;

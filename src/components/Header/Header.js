46edcd2c37e98b127fdbf92e5de8504107a74699
Natalia Jaguś <natalia.jagus@gmail.com>
2019-09-29 20:14:47 +0200

    create Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Header = styled.header`
+    background-image: linear-gradient(#15A1F8, #065B9A);
+    width: 100vw;
+    border-radius: 0px 0px 40% 40%;
+    padding: 50px 0px 20px 0px;
+    display: flex;
+    min-height: 200px;
+    flex-direction: column;
+    align-items: center;
+    `;
+
+export default Header;

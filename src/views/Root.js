d76c3923a7cd945cf968df3823b1c391707ecd44
Natalia Jaguś <natalia.jagus@gmail.com>
2019-09-29 20:15:50 +0200

    add React Router DOM

---
+++ src/views/Root.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import GlobalStyle from 'theme/GlobalStyle.js';
+import Exercise from 'views/Exercise';
+import Weeks from 'views/Weeks';
+
+
+const Root = () => (
+  <>
+  <GlobalStyle />
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Weeks} />
+      <Route path="/exercise" component={Exercise} />
+    </Switch>
+  </BrowserRouter>
+  </>
+  
+)
+
+
+export default Root;

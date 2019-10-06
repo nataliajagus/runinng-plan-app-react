a1b4b20becd5d0279f3e0e979677ed8afbfceb4a
Natalia Jaguś <natalia.jagus@gmail.com>
2019-10-06 13:39:18 +0200

    add state for exercise

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,45 +1,47 @@
-import React from 'react';
-import styled from 'styled-components';
-
+import React from "react";
+import styled from "styled-components";
 
 const StyledContainer = styled.div`
-    padding: 50px;
-    background: #FFF;
-    flex-shrink: 0;
-    flex-direction: column;
-    display: flex;
-    max-width: 350px;
-    align-self: center;
-    border-radius: 18px;
-    border: none;
-    box-shadow: 1px 2px 8px 0px rgba(0,0,0,0.18);
-    margin-top: 30px;
-    justify-content: center;
-    align-items: center;
+  padding: 50px;
+  background: #fff;
+  flex-shrink: 0;
+  flex-direction: column;
+  display: flex;
+  max-width: 350px;
+  align-self: center;
+  border-radius: 18px;
+  border: none;
+  box-shadow: 1px 2px 8px 0px rgba(0, 0, 0, 0.18);
+  margin-top: 30px;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Time = styled.span`
-    color: #1F5AAC;
-    display: flex;
-    font-size: 70px;
-    justify-content: center;
-    font-weight: 700;
-    min-width: 300px;
+  color: #1f5aac;
+  display: flex;
+  font-size: 70px;
+  justify-content: center;
+  font-weight: 700;
+  min-width: 300px;
 `;
 
 const Mode = styled.span`
-    font-size: 50px;
-    font-weight: 300;
-    color: #1F5AAC;
-    text-align: center;
+  font-size: 50px;
+  font-weight: 300;
+  color: #1f5aac;
+  text-align: center;
 `;
 
-
-const Timer = () => (
+const Timer = props => {
+  return (
     <StyledContainer>
-        <Time>00:00</Time>
-        <Mode>run</Mode>
+      <Time>
+        {props.minutes} : {props.seconds}
+      </Time>
+      <Mode>run</Mode>
     </StyledContainer>
-)
+  );
+};
 
 export default Timer;
--- src/views/Exercise.js
+++ src/views/Exercise.js
@@ -1,42 +1,101 @@
-import React from 'react';
-import Timer from 'components/Timer/Timer';
-import Header from 'components/Header/Header';
-import Controls from 'components/Controls/Controls';
-import Wrapper from 'components/Wrapper/Wrapper';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import Timer from "components/Timer/Timer";
+import Header from "components/Header/Header";
+import Wrapper from "components/Wrapper/Wrapper";
+import styled from "styled-components";
+import Button from "components/Button/Button";
 
 const WeekTitle = styled.h2`
-    color: #fff;
-    display: flex;
-    flex-direction: column;
-    text-align: left;
-    font-size: 35px;
-    margin-top: -20px;
-    align-self: center;
-    margin-top: 20px;
-    text-align: center;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  font-size: 35px;
+  margin-top: -20px;
+  align-self: center;
+  margin-top: 20px;
+  text-align: center;
 `;
 
 const Subtitle = styled.p`
-    color: #fff;
-    text-align: center;
-    font-weight: 100;
+  color: #fff;
+  text-align: center;
+  font-weight: 100;
 `;
 
-function Exercise() {
-  return (
-    <div>
-      <Wrapper>
-      <Header>
-      <WeekTitle>Week 1</WeekTitle>
-      <Subtitle>Time left: 30:00</Subtitle>
-      <Timer />
-      </Header>
-      <Controls />
-      </Wrapper>
-    </div>
+class Exercise extends Component {
+  state = {
+    timerOn: false,
+    timerStart: 0,
+    timerTime: 10000
+  };
 
-  );
+  startTimer = () => {
+    this.setState({
+      timerOn: true,
+      timerTime: this.state.timerTime,
+      timerStart: this.state.timerTime
+    });
+    this.timer = setInterval(() => {
+      const newTime = this.state.timerTime - 10;
+      if (newTime >= 0) {
+        this.setState({
+          timerTime: newTime
+        });
+      } else {
+        clearInterval(this.timer);
+        this.setState({ timerOn: false });
+        alert("Countdown ended");
+      }
+    }, 10);
+  };
+
+  stopTimer = () => {
+    clearInterval(this.timer);
+    this.setState({ timerOn: false });
+  };
+  resetTimer = () => {
+    if (this.state.timerOn === false) {
+      this.setState({
+        timerTime: this.state.timerStart
+      });
+    }
+  };
+
+  render() {
+    const { timerTime, timerStart, timerOn } = this.state;
+    let seconds = ("0" + (Math.floor((timerTime / 1000) % 60) % 60)).slice(-2);
+    let minutes = ("0" + Math.floor((timerTime / 60000) % 60)).slice(-2);
+
+    return (
+      <Wrapper>
+        <Header>
+          <WeekTitle>Week 1</WeekTitle>
+          <Subtitle>Time left: 30:00</Subtitle>
+          <Timer minutes={minutes} seconds={seconds} />
+        </Header>
+        <div>
+          {timerOn === false &&
+            (timerStart === 0 || timerTime === timerStart) && (
+              <Button onClick={this.startTimer}>Start</Button>
+            )}
+          {timerOn === true && timerTime >= 1000 && (
+            <Button stop onClick={this.stopTimer}>Stop</Button>
+          )}
+          {timerOn === false &&
+            (timerStart !== 0 &&
+              timerStart !== timerTime &&
+              timerTime !== 0) && (
+              <Button onClick={this.startTimer}>Resume</Button>
+            )}
+          {(timerOn === false || timerTime < 1000) &&
+            (timerStart !== timerTime && timerStart > 0) && (
+              <Button onClick={this.resetTimer}>Reset</Button>
+            )}
+        </div>
+      </Wrapper>
+    );
+  }
 }
 
 export default Exercise;

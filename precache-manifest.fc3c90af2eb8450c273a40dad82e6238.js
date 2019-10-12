115eba9446dd3ccecb13171c7c2ae61751edb5dd
Natalia Jaguś <natalia.jagus@gmail.com>
2019-10-12 11:39:48 +0200

    Updates

---
+++ precache-manifest.fc3c90af2eb8450c273a40dad82e6238.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d58bf62e65f8c9aa0fc7eca570450e6",
+    "url": "/runinng-plan-app-react/index.html"
+  },
+  {
+    "revision": "4f4ef651a52c25fcc89d",
+    "url": "/runinng-plan-app-react/static/js/2.72c9c6f1.chunk.js"
+  },
+  {
+    "revision": "67ab31251fa0505fd8ff",
+    "url": "/runinng-plan-app-react/static/js/main.9f732112.chunk.js"
+  },
+  {
+    "revision": "0e1108a44dc3a3f3dc2f",
+    "url": "/runinng-plan-app-react/static/js/runtime~main.64e86191.js"
+  },
+  {
+    "revision": "b7a278f8c90c5d3dc06775b7a2ff7a74",
+    "url": "/runinng-plan-app-react/static/media/jogging.b7a278f8.svg"
+  }
+]);

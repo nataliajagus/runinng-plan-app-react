5db363cfcbd33b5067d6c601478b43122ea9fcb2
Natalia Jaguś <natalia.jagus@gmail.com>
2019-10-12 11:17:06 +0200

    add proper time for each week

--- src/settings.js
+++ src/settings.js
@@ -1,45 +1,63 @@
 export const settings = [
     {   
         week: 1,
+        run: 120000,
+        walk: 240000,
+        rounds: 5
+    },
+    {
+        week: 2,
+        run: 180000,
+        walk: 180000,
+        rounds: 5
+    },
+    {
+        week: 3,
+        run: 180000,
+        walk: 150000,
+        rounds: 4
+    },
+    {
+        week: 4,
+        run: 420000,
+        walk: 180000,
+        rounds: 3
+    },
+    {
+        week: 5,
         run: 10000,
         walk: 5000,
         rounds: 2
     },
     {
-        week: 2,
-        seconds: 2000
-    },
-    {
-        week: 3,
-        econds: 3000
-    },
-    {
-        week: 4,
-        seconds: 4000
-    },
-    {
-        week: 5,
-        seconds: 5000
-    },
-    {
         week: 6,
-        seconds: 6000
+        run: 480000,
+        walk: 120000,
+        rounds: 3
     },
     {
         week: 7,
-        seconds: 7000
+        run: 540000,
+        walk: 60000,
+        rounds: 3
     },
     {
         week: 8,
-        seconds: 8000
+        run: 780000,
+        walk: 120000,
+        rounds: 2
     },
     {
         week: 9,
-        seconds: 9000
+        run: 840000,
+        walk: 60000,
+        rounds: 2
     },
     {
         week: 10,
-        seconds: 10000
+        run: 1800000,
+        walk: 0,
+        rounds: 1
     }
 ]
 

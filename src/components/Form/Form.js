ad499bf60e0d226c75d3d05f2d56742cc1e63735
Natalia Jaguś <natalia.jagus@gmail.com>
2019-10-17 11:33:13 +0200

    fix custom exercise form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -14,23 +14,14 @@ const StyledForm = styled.form`
 
 class Form extends Component {
 
-    state = {
-        runMinutes: 0,
-        runSeconds: 0,
-        walkMinutes: 0,
-        walkSeconds: 0,
-        rounds: 0
-      }
 
-    createExercise = (e) => {
-        e.preventDefault();
-        console.log(e.target[0].value);
-        this.setState({ runMinutes: e.target[0].value, runSeconds: e.target[1].value, walkMinutes: e.target[2].value, walkSeconds: e.target[3].value, rounds: e.target[4].value });
-      }
 
     render() {
+
+        const {rounds, runMinutes, runSeconds, walkMinutes, walkSeconds} = this.props.state;
+
         return (
-            <StyledForm onSubmit={this.createExercise}>
+            <StyledForm onSubmit={this.props.submitFn}>
                 <h4 className={styles.formHeading}>Running interval</h4>
                 <div className={styles.optionWrapper}>
                     <div className={styles.inputWrapper}>
@@ -56,7 +47,11 @@ class Form extends Component {
                 
                 <h4 className={styles.formHeading}>Rounds</h4>
                 <input type="number" name="rounds" placeholder="0"  className={styles.formInput} />
-                <Link to={{ pathname: '/exercise', state: { run: ((this.state.runMinutes * 60) + this.state.runSeconds) * 1000, walk: ((this.state.walkMinutes * 60) + this.state.walkSeconds) * 1000, rounds: this.state.rounds, week: 0 } }}><Button htmlType="submit" submit>Create exercise</Button></Link>
+
+                
+
+                {rounds == 0 && (runMinutes + runSeconds + walkMinutes + walkSeconds) == 0 && ( <Button htmlType="submit" submit>Create exercise</Button> )}
+
             </StyledForm>
         )
     }
--- src/views/Weeks.js
+++ src/views/Weeks.js
@@ -6,6 +6,8 @@ import Wrapper from "components/Wrapper/Wrapper";
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import { settings } from "settings";
+import Form from "components/Form/Form";
+import Button from "components/Button/Button";
 
 const WeeksWrapper = styled.div`
   flex-flow: row;
@@ -15,9 +17,33 @@ const WeeksWrapper = styled.div`
   z-index: 2;
   display: flex;
   max-width: 800px;
+  margin-bottom: 50px;
 `;
 
 class Weeks extends Component {
+
+  state = {
+    runMinutes: 0,
+    runSeconds: 0,
+    walkMinutes: 0,
+    walkSeconds: 0,
+    rounds: 0
+  }
+
+  createExercise = (e) => {
+    e.preventDefault();
+    this.setState({ runMinutes: e.target[0].value, runSeconds: e.target[1].value, walkMinutes: e.target[2].value, walkSeconds: e.target[3].value, rounds: e.target[4].value });
+    console.log(this.state);
+
+    if(!e.target[4].value) {
+        alert('add data!');  
+    } 
+    
+}
+
+componentDidUpdate() {
+  console.log(this.state);
+}
 
   render() {
     return (
@@ -32,6 +58,10 @@ class Weeks extends Component {
         ))}
 
         </WeeksWrapper>
+        <h3>or create your own exercise:</h3>
+        <Form submitFn={this.createExercise} state={this.state} />
+
+        {this.state.rounds != 0 && (this.state.runMinutes + this.state.runSeconds + this.state.walkMinutes + this.state.walkSeconds) != 0 && ( <Link  to={{ pathname: '/exercise', state: { run: ((this.state.runMinutes * 60) + this.state.runSeconds) * 1000, walk: ((this.state.walkMinutes * 60) + this.state.walkSeconds) * 1000, rounds: this.state.rounds, week: 0 } }}><Button htmlType="submit" submit>Continue</Button></Link> )}
       </Wrapper>
     );
   }

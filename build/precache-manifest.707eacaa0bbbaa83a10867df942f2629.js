d337d6b3ddf1fce3c0008cd860e14ba2e165f11e
Natalia Jaguś <natalia.jagus@gmail.com>
2019-10-22 21:39:08 +0200

    add 404.html

---
+++ build/precache-manifest.707eacaa0bbbaa83a10867df942f2629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde73722863bd7b6608c7ab21df9ed33",
+    "url": "/runinng-plan-app-react/index.html"
+  },
+  {
+    "revision": "fc245c811b631c464d82",
+    "url": "/runinng-plan-app-react/static/css/main.193ed157.chunk.css"
+  },
+  {
+    "revision": "f9de891badb75a2993d4",
+    "url": "/runinng-plan-app-react/static/js/2.d936576f.chunk.js"
+  },
+  {
+    "revision": "fc245c811b631c464d82",
+    "url": "/runinng-plan-app-react/static/js/main.7188578b.chunk.js"
+  },
+  {
+    "revision": "0e1108a44dc3a3f3dc2f",
+    "url": "/runinng-plan-app-react/static/js/runtime~main.64e86191.js"
+  },
+  {
+    "revision": "b7a278f8c90c5d3dc06775b7a2ff7a74",
+    "url": "/runinng-plan-app-react/static/media/jogging.b7a278f8.svg"
+  }
+]);

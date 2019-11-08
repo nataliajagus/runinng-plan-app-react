c612f4558df99a3a705263539b7ffb35e0675811
Natalia Jaguś <natalia.jagus@gmail.com>
2019-11-08 23:50:07 +0100

    configure Formik and Yup

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,9 @@
 import React from "react";
-import ValidationInfo from "components/ValidationInfo/ValidationInfo";
 import styled from "styled-components";
 import Button from "components/Button/Button";
+import { Formik } from "formik";
+import * as Yup from 'yup';
+import ValidationInfo from "components/ValidationInfo/ValidationInfo";
 
 const StyledForm = styled.form`
   justify-content: center;
@@ -14,7 +16,7 @@ const StyledInput = styled.input`
   width: 80px;
   height: 80px;
   border-radius: 15px;
-  border: 1px solid #1F5AAC;
+  border: 1px solid #1f5aac;
   font-size: 30px;
   font-weight: 900;
 
@@ -32,90 +34,123 @@ const OptionWrapper = styled.div`
   flex-direction: row;
 `;
 
-const InputWrapper = styled.div`
+const StyledLabel = styled.label`
   display: flex;
   flex-direction: column;
   margin: 0 20px;
 `;
 
+Yup.addMethod(Yup.mixed, 'oneOfRequired', function() {
+  return this.test(
+    'oneOfRequired',
+    'One of intervals must be entered',
+    function() {
+      return (this.parent.runMinutes || this.parent.runSeconds || this.parent.walkMinutes || this.parent.walkSeconds)
+    }
+  );
+});
 
-const Form = props => {
+const exerciseSchema = Yup.object().shape({
+  runMinutes: Yup.number().oneOfRequired(),
+  runSeconds: Yup.number().oneOfRequired(),
+  walkMinutes: Yup.number().oneOfRequired(),
+  walkSeconds: Yup.number().oneOfRequired(),
+  rounds: Yup
+  .number()
+  .min(1)
+  .required()
+})
+
+const Form = ({ submitFn, submitted }) => {
   return (
-    <StyledForm onSubmit={props.submitFn}>
-      <h4>Running interval</h4>
-      <OptionWrapper>
-        <InputWrapper>
-          <StyledInput
-            type="number"
-            name="runMinutes"
-            placeholder="0"
-            min="0"
-            max="60"
-          />
-          <label htmlFor="runMinutes">
-            minutes
-          </label>
-        </InputWrapper>
-        <InputWrapper>
-          <StyledInput
-            type="number"
-            name="runSeconds"
-            placeholder="0"
-            min="0"
-            max="60"
-          />
-          <label htmlFor="runSeconds">
-            seconds
-          </label>
-        </InputWrapper>
-      </OptionWrapper>
-      <h4>Walking interval</h4>
-      <OptionWrapper>
-        <InputWrapper>
-          <StyledInput
-            type="number"
-            name="walkMinutes"
-            placeholder="0"
-            min="0"
-            max="60"
-          />
-          <label htmlFor="walkMinutes">
-            minutes
-          </label>
-        </InputWrapper>
-        <InputWrapper>
-          <StyledInput
-            type="number"
-            name="walkSeconds"
-            placeholder="0"
-            min="0"
-            max="60"
-          />
-          <label htmlFor="walkSeconds">
-            seconds
-          </label>
-        </InputWrapper>
-      </OptionWrapper>
-
-      <h4>Rounds</h4>
-      <StyledInput
-        type="number"
-        name="rounds"
-        placeholder="0"
+    <>
+      <Formik
+        initialValues={{
+          runMinutes: '',
+          runSeconds: '',
+          walkMinutes: '',
+          walkSeconds: '',
+          rounds: ''
+        }}
+        validationSchema={exerciseSchema}
+        onSubmit={submitFn}
+        render={({
+          values,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          errors,
+          touched
+        }) => (
+          <StyledForm onSubmit={handleSubmit}>
+            <h4>Running interval</h4>
+            <OptionWrapper>
+              <StyledLabel>
+                <StyledInput
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.runMinutes}
+                  type="number"
+                  name="runMinutes"
+                  placeholder="0"
+                />
+                minutes
+              </StyledLabel>
+              <StyledLabel>
+                <StyledInput
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.runSeconds}
+                  type="number"
+                  name="runSeconds"
+                  placeholder="0"
+                />
+                seconds
+              </StyledLabel>
+            </OptionWrapper>
+            <h4>Walking interval</h4>
+            <OptionWrapper>
+              <StyledLabel>
+                <StyledInput
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.walkMinutes}
+                  type="number"
+                  name="walkMinutes"
+                  placeholder="0"
+                />
+                minutes
+              </StyledLabel>
+              <StyledLabel>
+                <StyledInput
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.walkSeconds}
+                  type="number"
+                  name="walkSeconds"
+                  placeholder="0"
+                />
+                seconds
+              </StyledLabel>
+            </OptionWrapper>
+            <h4>Rounds</h4>
+            <StyledInput
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.rounds}
+              type="number"
+              name="rounds"
+              placeholder="0"
+            />
+            {touched.runMinutes && errors.runMinutes && <ValidationInfo warning>Add some data to create the exercise :)</ValidationInfo>}
+            {touched.rounds && errors.rounds && <ValidationInfo warning>Your exercise should have at least one round</ValidationInfo>}
+            <Button type="submit" submit invisible={submitted}>
+              Create exercise
+            </Button>
+          </StyledForm>
+        )}
       />
-
-      {props.state.isValid === false && (
-        <ValidationInfo warning>
-          Please enter some data to create your exercise :)
-        </ValidationInfo>
-      )}
-
-      {!props.state.isValid && (
-        <Button htmlType="submit" submit>
-          Create exercise
-        </Button>
-      )}
-    </StyledForm>
+    </>
   );
 };
 
